ec2719e9f99f23deae98b72f67c77708cc549267
Sergei Iu <serg_omen@mail.ru>
2022-05-12 12:18:17 +0300

    posts

--- auth/urls.py
+++ auth/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('home', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
+    path('create-post', views.create_post, name='create_post'),
 ]
--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, 
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="author_name",
 on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
