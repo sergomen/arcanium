479da5f879598ef98c43df131006fee40a641efd
Sergei Iu <serg_omen@mail.ru>
2022-05-16 22:48:26 +0300

    Heroku

--- arcanium/settings.py
+++ arcanium/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd7mjph2vco40v9',
         'USER': 'wwladqyxgxrrqj',
-        'PASSWORD': '104ef63a99f5eb6eb5ba4603cf5139307e6a5f36b4e71e330aca8a0ca3c7e835' #get_secret('DB_PASSWORD'),
+        'PASSWORD': '104ef63a99f5eb6eb5ba4603cf5139307e6a5f36b4e71e330aca8a0ca3c7e835', #get_secret('DB_PASSWORD'),
         'HOST': 'ec2-34-194-73-236.compute-1.amazonaws.com',
         'PORT': '5432',
     }
